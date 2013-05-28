64e5969a016face0e4532433f5eee7823deb357e
琚雪 <juxue.cy@taobao.com>
2013-05-28 11:59:12 +0800

    Initial Commit

---
+++ RunLoop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunLoop
+//
+//  Created by ChenYang on 13-5-28.
+//  Copyright (c) 2013年 cy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ RunLoop/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  RunLoop
+//
+//  Created by ChenYang on 13-5-28.
+//  Copyright (c) 2013年 cy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
